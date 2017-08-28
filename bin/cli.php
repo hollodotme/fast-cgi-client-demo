27fc99d24e420d2264091b32783df8168d587305
Holger Woltersdorf <hw@hollo.me>
2017-08-28 19:19:03 +0200

    Removes old examples, replaces vagrant with docker compose

---
+++ bin/cli.php
@@ -0,0 +1,11 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+/**
+ * @author hollodotme
+ */
+
+namespace hollodotme\FastCGI\ClienDemo\Bin;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
