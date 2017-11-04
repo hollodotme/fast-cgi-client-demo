a93fe63176250dafe7b6887bd9df858c65ccd694
Holger Woltersdorf <hw@hollo.me>
2017-11-04 11:56:42 +0100

    Updates demo frontend

---
+++ public/js/theme.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+    if (!!window.EventSource) {
+        var logOutput = $('#processes');
+        logOutput.text('');
+        var source = new EventSource('/showProcesses.php');
+
+        source.addEventListener('beginOfStream', function () {
+            logOutput.append("Stream started.\n");
+        }, false);
+
+        source.addEventListener('open', function () {
+            console.log('Event Source opened.');
+        }, false);
+
+        source.addEventListener('clear', function (e) {
+            logOutput.html('');
+        }, false);
+
+        source.addEventListener('message', function (e) {
+            logOutput.append(e.data + "\n");
+        }, false);
+
+        source.addEventListener('endOfStream', function () {
+            source.close();
+            logOutput.append("Stream ended.\n");
+        }, false);
+
+        source.addEventListener('error', function (e) {
+            if (e.readyState === EventSource.CLOSED) {
+                console.log('Event Source closed.');
+                source.close();
+            }
+        }, false);
+    }
+    else {
+        alert('No event source available. Please use another, modern browser!');
+    }
+
+    $('#left').find('button').click(function (e) {
+        e.preventDefault();
+        var callMethod = $(this).attr('id');
+        if (!!window.EventSource) {
+            var output = $('#output');
+            output.text('');
+            var es = new EventSource('/createPDFs.php?callMethod=' + callMethod);
+
+            es.addEventListener('beginOfStream', function () {
+                output.append("Stream started.\n");
+            }, false);
+
+            es.addEventListener('open', function () {
+                console.log('Event Source opened.');
+            }, false);
+
+            es.addEventListener('message', function (e) {
+                output.append(e.data + "\n");
+                output.scrollTop(9999999);
+            }, false);
+
+            es.addEventListener('endOfStream', function () {
+                output.append("Stream ended.\n");
+                output.scrollTop(9999999);
+                es.close();
+            }, false);
+
+            es.addEventListener('error', function (e) {
+                if (e.readyState === EventSource.CLOSED) {
+                    console.log('Event Source closed.');
+                    es.close();
+                }
+            }, false);
+        }
+        else {
+            alert('No event source available. Please use another, modern browser!');
+        }
+    });
+});
