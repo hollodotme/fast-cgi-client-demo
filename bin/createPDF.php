46ea5d53770770ebaef976e12461a569bd4d0cfc
Holger Woltersdorf <hw@hollo.me>
2017-11-04 11:55:32 +0100

    Renames pdf creation worker

--- bin/createPDF.php
+++ bin/createPDF.php
@@ -6,26 +6,26 @@
 error_reporting( E_ALL );
 ini_set( 'display_errors', 'On' );
 
-$documentId = $_POST['documentId'] ?? $argv[1];
-$tempfile   = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $documentId . '.html';
+$documentId = $_POST['documentId'];
+$tempFile   = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $documentId . '.html';
 $outputFile = '/vagrant/public/documents/' . $documentId . '.pdf';
 
 $htmlContent = file_get_contents( __DIR__ . '/Template.html' );
 $htmlContent = str_replace( '[DOCUMENT_ID]', $documentId, $htmlContent );
-file_put_contents( $tempfile, $htmlContent );
-chmod( $tempfile, 0777 );
+file_put_contents( $tempFile, $htmlContent );
+chmod( $tempFile, 0777 );
 
 $command = sprintf(
 	'xvfb-run wkhtmltopdf %s %s',
-	escapeshellarg( 'file://' . $tempfile ),
+	escapeshellarg( 'file://' . $tempFile ),
 	escapeshellarg( $outputFile )
 );
 
-sleep( random_int( 2, 6 ) );
+sleep( random_int( 1, 3 ) );
 
 shell_exec( $command );
 
-@unlink( $tempfile );
+@unlink( $tempFile );
 
 echo sprintf(
 	'<a href="http://demo.fast-cgi-client.de/documents/%s.pdf" target="_blank">%s.pdf</a>',
